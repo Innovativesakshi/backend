cbcd21b865440095afbbc313060ad5b00dc42c44
Sakshi <tripathisakshi6392@gmail.com>
2026-01-23 07:21:20 +0530

    Fix: Add certifi and dnspython for MongoDB SSL connection

--- app/database.py
+++ app/database.py
@@ -2,12 +2,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
 
+import certifi
+
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "hrms_lite")
 
-client = AsyncIOMotorClient(MONGO_URI)
+# Add tlsCAFile=certifi.where() to fix SSL handshake errors on cloud platforms/Render
+client = AsyncIOMotorClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client[DB_NAME]
 
 async def get_database():
